bd8b898fe4c82095c1deccbd61c35e8a64dda25a
zeyaladag <zeyaladag@gmail.com>
2024-11-10 16:46:57 +0300

    first week's code files added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// creating a new object from the initial sample
+var trackingSystem = new Homework();
+
+//setting fields
+trackingSystem.Id = Guid.NewGuid();
+trackingSystem.Title = "Inventory Tracking System";
+trackingSystem.Summary = "Stock Control";
+trackingSystem.Level = 70;
+
+//print out
+Console.WriteLine("{0} (L:{2})\n\t{1}",trackingSystem.Title,trackingSystem.Summary,trackingSystem.Level);
+//{0} is a placeholder for trackingSystem,like %d in java. it maps the variables.
+
+// this is an initial sample. it has default behaviours that can be overwritten
+class Homework {
+    public Guid Id;
+    public String Title;
+    public String Summary;
+    public short Level;
+
+}
